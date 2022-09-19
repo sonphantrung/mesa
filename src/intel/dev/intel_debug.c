42c7fd5f3869248121e31904dd61c91a5a7f814b
Rohan Garg <rohan.garg@intel.com>
2022-09-19 18:15:28 +0200

    anv: emit a resource barrier for transitions on xe2+
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>

--- src/intel/dev/intel_debug.c
+++ src/intel/dev/intel_debug.c
@@ -106,6 +106,7 @@ static const struct debug_control debug_control[] = {
    { "draw_bkp",    DEBUG_DRAW_BKP },
    { "bat-stats",   DEBUG_BATCH_STATS },
    { "reg-pressure", DEBUG_REG_PRESSURE },
+   { "usebarriers", DEBUG_USEBARRIERS },
    { NULL,    0 }
 };
 

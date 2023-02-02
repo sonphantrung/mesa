d3ad49560f44ca603b766f9a63c156e50012f1ff
Eric Engestrom <eric@igalia.com>
2023-02-02 17:03:17 +0000

    v3d: keep old buffer content to allow using buffer_age
    This seems to only cause problems on Android, but dropping the clear
    doesn't cause any issue on Wayland or X11.
    
    Signed-off-by: Eric Engestrom <eric@igalia.com>

--- src/gallium/drivers/v3d/v3d_job.c
+++ src/gallium/drivers/v3d/v3d_job.c
@@ -393,17 +393,6 @@ v3d_get_job_for_fbo(struct v3d_context *v3d)
          */
         v3d->dirty = ~0;
 
-        /* If we're binding to uninitialized buffers, no need to load their
-         * contents before drawing.
-         */
-        for (int i = 0; i < nr_cbufs; i++) {
-                if (cbufs[i]) {
-                        struct v3d_resource *rsc = v3d_resource(cbufs[i]->texture);
-                        if (!rsc->writes)
-                                job->clear |= PIPE_CLEAR_COLOR0 << i;
-                }
-        }
-
         if (zsbuf) {
                 struct v3d_resource *rsc = v3d_resource(zsbuf->texture);
                 if (!rsc->writes)

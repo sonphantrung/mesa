634e3b8f4ad4df94a60d853ce2c7927c82de03a1
Connor Abbott <cwabbott0@gmail.com>
2023-03-16 20:58:43 +0100

    tu: Implement early preamble

--- src/freedreno/vulkan/tu_shader.cc
+++ src/freedreno/vulkan/tu_shader.cc
@@ -1204,6 +1204,7 @@ tu6_emit_xs(struct tu_cs *cs,
                .fullregfootprint = xs->info.max_reg + 1,
                .branchstack = ir3_shader_branchstack_hw(xs),
                .mergedregs = xs->mergedregs,
+               .earlypreamble = xs->info.early_preamble,
       ));
       break;
    case MESA_SHADER_TESS_CTRL:
@@ -1211,6 +1212,7 @@ tu6_emit_xs(struct tu_cs *cs,
                .halfregfootprint = xs->info.max_half_reg + 1,
                .fullregfootprint = xs->info.max_reg + 1,
                .branchstack = ir3_shader_branchstack_hw(xs),
+               .earlypreamble = xs->info.early_preamble,
       ));
       break;
    case MESA_SHADER_TESS_EVAL:
@@ -1218,6 +1220,7 @@ tu6_emit_xs(struct tu_cs *cs,
                .halfregfootprint = xs->info.max_half_reg + 1,
                .fullregfootprint = xs->info.max_reg + 1,
                .branchstack = ir3_shader_branchstack_hw(xs),
+               .earlypreamble = xs->info.early_preamble,
       ));
       break;
    case MESA_SHADER_GEOMETRY:
@@ -1225,6 +1228,7 @@ tu6_emit_xs(struct tu_cs *cs,
                .halfregfootprint = xs->info.max_half_reg + 1,
                .fullregfootprint = xs->info.max_reg + 1,
                .branchstack = ir3_shader_branchstack_hw(xs),
+               .earlypreamble = xs->info.early_preamble,
       ));
       break;
    case MESA_SHADER_FRAGMENT:
@@ -1238,6 +1242,7 @@ tu6_emit_xs(struct tu_cs *cs,
                /* unknown bit, seems unnecessary */
                .unk24 = true,
                .pixlodenable = xs->need_pixlod,
+               .earlypreamble = xs->info.early_preamble,
                .mergedregs = xs->mergedregs,
       ));
       break;
@@ -1249,6 +1254,7 @@ tu6_emit_xs(struct tu_cs *cs,
                .fullregfootprint = xs->info.max_reg + 1,
                .branchstack = ir3_shader_branchstack_hw(xs),
                .threadsize = thrsz,
+               .earlypreamble = xs->info.early_preamble,
                .mergedregs = xs->mergedregs,
       ));
       break;

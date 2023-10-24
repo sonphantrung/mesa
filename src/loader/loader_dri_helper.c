2e3a30ca6312889c8f787ab8f5892d590357d558
Emma Anholt <emma@anholt.net>
2023-10-24 15:33:00 -0700

    dri: Consistently use createImageWithModifiers2().
    dri_screen.c supports it regardless.

--- src/loader/loader_dri_helper.c
+++ src/loader/loader_dri_helper.c
@@ -57,15 +57,10 @@ __DRIimage *loader_dri_create_image(__DRIscreen *screen,
       if (!has_valid_modifier)
          return NULL;
 
-      if (image->createImageWithModifiers2)
-         return image->createImageWithModifiers2(screen, width, height,
-                                                 dri_format, modifiers,
-                                                 modifiers_count, dri_usage,
-                                                 loaderPrivate);
-      else
-         return image->createImageWithModifiers(screen, width, height,
-                                                dri_format, modifiers,
-                                                modifiers_count, loaderPrivate);
+      return image->createImageWithModifiers2(screen, width, height,
+                                              dri_format, modifiers,
+                                              modifiers_count, dri_usage,
+                                              loaderPrivate);
    }
 
    /* No modifier given or fallback to the legacy createImage allowed */

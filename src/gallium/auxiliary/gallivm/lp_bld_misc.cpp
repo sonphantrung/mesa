Grima04/mesa-turnip-kgsl

commit e983a975c6843c307380d7caa083eee89e02bd3c

gallivm: disable NEON instructions if they are not supported
The LLVM project made some questionable decisions about defaults for
armv7 (e.g. they enable NEON that is not there on NVIDIA and Marvell
platforms).

On top of that, getHostCPUFeatures() doesn't disable missing machine
attributes. Finally, -neon alone is not sufficient to disable emmision
of NEON instructions.

Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
Cc: <mesa-stable@lists.freedesktop.org>
Reviewed-by: Matt Turner <mattst88@gmail.com>

src/gallium/auxiliary/gallivm/lp_bld_misc.cpp
@@ -625,6 +625,13 @@ lp_build_create_jit_compiler_for_module(LLVMExecutionEngineRef *OutJIT,
    MAttrs.push_back("-avx512vl");
 #endif
 #endif
+#if defined(PIPE_ARCH_ARM)
+   if (!util_cpu_caps.has_neon) {
+      MAttrs.push_back("-neon");
+      MAttrs.push_back("-crypto");
+      MAttrs.push_back("-vfp2");
+   }
+#endif
 
 #if defined(PIPE_ARCH_PPC)
    MAttrs.push_back(util_cpu_caps.has_altivec ? "+altivec" : "-altivec");

sonphantrung/mesa

commit 454677c6e24a47fb4811b3b589a9b8897b6652e0

llvmpipe: Fix armhfp without neon
Most armhfp distributions are using armv7-a with -mfpu=vfp3-d16 with neon optional.

When the CPU lacks neon, such as Tegra20 and Marvell Dove, llvm needs to have -d32 feature removed in order not to assume vfp3-d32 fpu

Fixes: e983a975c6 ("gallivm: disable NEON instructions if they are not
supported")
Closes: #7868
Signed-off-by: Nicolas Chauvet <kwizart@gmail.com>

src/gallium/auxiliary/gallivm/lp_bld_misc.cpp
@@ -441,8 +441,8 @@ lp_build_create_jit_compiler_for_module(LLVMExecutionEngineRef *OutJIT,
 #if DETECT_ARCH_ARM
    if (!util_get_cpu_caps()->has_neon) {
       MAttrs.push_back("-neon");
-      MAttrs.push_back("-crypto");
-      MAttrs.push_back("-vfp2");
+      MAttrs.push_back("+fp64");
+      MAttrs.push_back("-d32");
    }
 #endif
 
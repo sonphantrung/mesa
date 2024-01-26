aerisarn/mesa-uwp

commit bf7777a5d4f42515a1bd9f681e28f74bfa2cc1de

lavapipe: set non-zero device/driver uuid
Closes #5875

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15230>

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -572,12 +572,14 @@ static void
 llvmpipe_get_driver_uuid(struct pipe_screen *pscreen, char *uuid)
 {
    memset(uuid, 0, PIPE_UUID_SIZE);
+   snprintf(uuid, PIPE_UUID_SIZE, "llvmpipeUUID");
 }
 
 static void
 llvmpipe_get_device_uuid(struct pipe_screen *pscreen, char *uuid)
 {
    memset(uuid, 0, PIPE_UUID_SIZE);
+   snprintf(uuid, PIPE_UUID_SIZE, "mesa" PACKAGE_VERSION);
 }
 
 static const struct nir_shader_compiler_options gallivm_nir_options = {

src/gallium/frontends/lavapipe/lvp_device.c
@@ -851,8 +851,8 @@ lvp_get_physical_device_properties_1_1(struct lvp_physical_device *pdevice,
 {
    assert(p->sType == VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_PROPERTIES);
 
-   memset(p->deviceUUID, 0, VK_UUID_SIZE);
-   memset(p->driverUUID, 0, VK_UUID_SIZE);
+   pdevice->pscreen->get_device_uuid(pdevice->pscreen, (char*)(p->deviceUUID));
+   pdevice->pscreen->get_driver_uuid(pdevice->pscreen, (char*)(p->driverUUID));
    memset(p->deviceLUID, 0, VK_LUID_SIZE);
    /* The LUID is for Windows. */
    p->deviceLUIDValid = false;

sonphantrung/mesa

commit 4507f0d7a29e1b1e15dd311628c6a8d7df3aae39

lavapipe: Fix format-truncation error
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10114
Fixes: bf7777a5d4f ("lavapipe: set non-zero device/driver uuid")
Tested-by: Marcus Seyfarth <m.seyfarth@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27256>

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -578,7 +578,7 @@ static void
 llvmpipe_get_device_uuid(struct pipe_screen *pscreen, char *uuid)
 {
    memset(uuid, 0, PIPE_UUID_SIZE);
-   snprintf(uuid, PIPE_UUID_SIZE, "mesa" PACKAGE_VERSION);
+   strncpy(uuid, "mesa" PACKAGE_VERSION, PIPE_UUID_SIZE);
 }
 
 
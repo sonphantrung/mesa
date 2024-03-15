8170b4d6277639c3024acd4bd525c3eee857d1c5
Connor Abbott <cwabbott0@gmail.com>
2024-03-15 10:41:49 -0400

    ir3: Plumb through ray_intersection intrinsic

--- src/freedreno/ir3/ir3.h
+++ src/freedreno/ir3/ir3.h
@@ -1116,6 +1116,7 @@ is_load(struct ir3_instruction *instr)
    case OPC_LDLW:
    case OPC_LDC:
    case OPC_LDLV:
+   case OPC_RAY_INTERSECTION:
       /* probably some others too.. */
       return true;
    default:
@@ -2739,6 +2740,7 @@ INSTR4(ATOMIC_S_OR)
 INSTR4(ATOMIC_S_XOR)
 #endif
 INSTR4NODST(LDG_K)
+INSTR5(RAY_INTERSECTION)
 
 /* cat7 instructions: */
 INSTR0(BAR)

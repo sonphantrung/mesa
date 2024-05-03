98e779a86b323c0a1c5452ee59ea0e9aac7e700a
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2024-05-03 17:46:36 +0200

    radv: enable virtio native context support

--- src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.c
+++ src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.c
@@ -205,14 +205,20 @@ radv_amdgpu_winsys_get_sync_types(struct radeon_winsys *rws)
 }
 
 struct radeon_winsys *
-radv_amdgpu_winsys_create(int fd, uint64_t debug_flags, uint64_t perftest_flags, bool reserve_vmid)
+radv_amdgpu_winsys_create(int fd, uint64_t debug_flags, uint64_t perftest_flags, bool reserve_vmid, bool is_virtio)
 {
    uint32_t drm_major, drm_minor, r;
    amdgpu_device_handle dev;
    struct radv_amdgpu_winsys *ws = NULL;
    struct libdrm_amdgpu *libdrm_amdgpu;
 
-   libdrm_amdgpu = ac_init_libdrm_amdgpu();
+  #if HAVE_AMDGPU_VIRTIO
+   if (is_virtio)
+      libdrm_amdgpu = ac_init_libdrm_amdgpu_for_virtio();
+   else
+#endif
+      libdrm_amdgpu = ac_init_libdrm_amdgpu();
+
    if (libdrm_amdgpu == NULL) {
       fprintf(stderr, "amdgpu: libdrm_amdgpu init failed");
       return NULL;
@@ -269,6 +275,7 @@ radv_amdgpu_winsys_create(int fd, uint64_t debug_flags, uint64_t perftest_flags,
    ws->libdrm_amdgpu = libdrm_amdgpu;
    ws->info.drm_major = drm_major;
    ws->info.drm_minor = drm_minor;
+   ws->info.is_virtio = is_virtio;
    if (!do_winsys_init(ws, fd))
       goto winsys_fail;
 
@@ -291,6 +298,12 @@ radv_amdgpu_winsys_create(int fd, uint64_t debug_flags, uint64_t perftest_flags,
    if (ws->syncobj_sync_type.features) {
       /* multi wait is always supported */
       ws->syncobj_sync_type.features |= VK_SYNC_FEATURE_GPU_MULTI_WAIT;
+
+      /* Disable timeline feature. */
+      if (!ws->info.has_timeline_syncobj) {
+         ws->syncobj_sync_type.get_value = NULL;
+         ws->syncobj_sync_type.features &= ~VK_SYNC_FEATURE_TIMELINE;
+      }
 
       ws->sync_types[num_sync_types++] = &ws->syncobj_sync_type;
       if (!(ws->syncobj_sync_type.features & VK_SYNC_FEATURE_TIMELINE)) {

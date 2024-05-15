aerisarn/mesa-uwp

commit be4287c3aa06b7f22ba83696108759f5fcd58323

pipe: Extend get_feedback with additional metadata
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26223>

src/gallium/frontends/va/buffer.c
@@ -195,9 +195,44 @@ VAStatus vlVaMapBuffer2(VADriverContextP ctx, VABufferID buf_id,
          return VA_STATUS_ERROR_INVALID_BUFFER;
 
       if (buf->type == VAEncCodedBufferType) {
-         ((VACodedBufferSegment*)buf->data)->buf = *pbuff;
-         ((VACodedBufferSegment*)buf->data)->size = buf->coded_size;
-         *pbuff = buf->data;
+         VACodedBufferSegment* curr_buf_ptr = (VACodedBufferSegment*) buf->data;
+
+         if ((buf->extended_metadata.present_metadata & PIPE_VIDEO_FEEDBACK_METADATA_TYPE_ENCODE_RESULT) &&
+             (buf->extended_metadata.encode_result & PIPE_VIDEO_FEEDBACK_METADATA_ENCODE_FLAG_FAILED)) {
+            curr_buf_ptr->status = VA_CODED_BUF_STATUS_BAD_BITSTREAM;
+            return VA_STATUS_ERROR_OPERATION_FAILED;
+         }
+
+         if (buf->extended_metadata.encode_result & PIPE_VIDEO_FEEDBACK_METADATA_ENCODE_FLAG_MAX_FRAME_SIZE_OVERFLOW)
+               curr_buf_ptr->status |= VA_CODED_BUF_STATUS_FRAME_SIZE_OVERFLOW;
+
+         if ((buf->extended_metadata.present_metadata & PIPE_VIDEO_FEEDBACK_METADATA_TYPE_CODEC_UNIT_LOCATION) == 0) {
+            curr_buf_ptr->buf = *pbuff;
+            curr_buf_ptr->size = buf->coded_size;
+            *pbuff = buf->data;
+         } else {
+            uint8_t* compressed_bitstream_data = *pbuff;
+            *pbuff = buf->data;
+
+            for (size_t i = 0; i < buf->extended_metadata.codec_unit_metadata_count - 1; i++) {
+               curr_buf_ptr->next = CALLOC(1, sizeof(VACodedBufferSegment));
+               if (!curr_buf_ptr->next)
+                  return VA_STATUS_ERROR_ALLOCATION_FAILED;
+               curr_buf_ptr = curr_buf_ptr->next;
+            }
+            curr_buf_ptr->next = NULL;
+
+            curr_buf_ptr = buf->data;
+            for (size_t i = 0; i < buf->extended_metadata.codec_unit_metadata_count; i++) {
+               curr_buf_ptr->status = VA_CODED_BUF_STATUS_SINGLE_NALU;
+               curr_buf_ptr->size = buf->extended_metadata.codec_unit_metadata[i].size;
+               curr_buf_ptr->buf = compressed_bitstream_data + buf->extended_metadata.codec_unit_metadata[i].offset;
+               if (buf->extended_metadata.codec_unit_metadata[i].flags & PIPE_VIDEO_CODEC_UNIT_LOCATION_FLAG_MAX_SLICE_SIZE_OVERFLOW)
+                  curr_buf_ptr->status |= VA_CODED_BUF_STATUS_SLICE_OVERFLOW_MASK;
+
+               curr_buf_ptr = curr_buf_ptr->next;
+            }
+         }
       }
    } else {
       mtx_unlock(&drv->mutex);
@@ -278,7 +313,17 @@ vlVaDestroyBuffer(VADriverContextP ctx, VABufferID buf_id)
          buf->derived_image_buffer->destroy(buf->derived_image_buffer);
    }
 
-   FREE(buf->data);
+   if (buf->type == VAEncCodedBufferType) {
+      VACodedBufferSegment* node = buf->data;
+      while(!node) {
+         VACodedBufferSegment* next = (VACodedBufferSegment*) node->next;
+         FREE(node);
+         node = next;
+      }
+   } else {
+      FREE(buf->data);
+   }
+
    FREE(buf);
    handle_table_remove(VL_VA_DRIVER(ctx)->htab, buf_id);
    mtx_unlock(&drv->mutex);
@@ -534,7 +579,7 @@ vlVaSyncBuffer(VADriverContextP ctx, VABufferID buf_id, uint64_t timeout_ns)
    vlVaSurface* surf = handle_table_get(drv->htab, buf->associated_encode_input_surf);
 
    if ((buf->feedback) && (context->decoder->entrypoint == PIPE_VIDEO_ENTRYPOINT_ENCODE)) {
-      context->decoder->get_feedback(context->decoder, buf->feedback, &(buf->coded_size));
+      context->decoder->get_feedback(context->decoder, buf->feedback, &(buf->coded_size), &(buf->extended_metadata));
       buf->feedback = NULL;
       /* Also mark the associated render target (encode source texture) surface as done
          in case they call vaSyncSurface on it to avoid getting the feedback twice*/

src/gallium/auxiliary/driver_trace/tr_video.c
@@ -257,7 +257,10 @@ trace_video_codec_flush(struct pipe_video_codec *_codec)
 }
 
 static void
-trace_video_codec_get_feedback(struct pipe_video_codec *_codec, void *feedback, unsigned *size)
+trace_video_codec_get_feedback(struct pipe_video_codec *_codec,
+                               void *feedback,
+                               unsigned *size,
+                               struct pipe_enc_feedback_metadata* metadata)
 {
     struct trace_video_codec *tr_vcodec = trace_video_codec(_codec);
     struct pipe_video_codec *codec = tr_vcodec->video_codec;
@@ -268,7 +271,7 @@ trace_video_codec_get_feedback(struct pipe_video_codec *_codec, void *feedback, 
     trace_dump_arg(ptr, size);
     trace_dump_call_end();
 
-    codec->get_feedback(codec, feedback, size);
+    codec->get_feedback(codec, feedback, size, metadata);
 }
 
 static int

src/gallium/drivers/d3d12/d3d12_video_enc.cpp
@@ -2102,7 +2102,7 @@ d3d12_video_encoder_encode_bitstream(struct pipe_video_codec * codec,
 }
 
 void
-d3d12_video_encoder_get_feedback(struct pipe_video_codec *codec, void *feedback, unsigned *size)
+d3d12_video_encoder_get_feedback(struct pipe_video_codec *codec, void *feedback, unsigned *size, struct pipe_enc_feedback_metadata* metadata)
 {
    struct d3d12_video_encoder *pD3D12Enc = (struct d3d12_video_encoder *) codec;
    assert(pD3D12Enc);

src/gallium/drivers/d3d12/d3d12_video_enc.h
@@ -71,7 +71,7 @@ d3d12_video_encoder_encode_bitstream(struct pipe_video_codec * codec,
  * get encoder feedback
  */
 void
-d3d12_video_encoder_get_feedback(struct pipe_video_codec *codec, void *feedback, unsigned *size);
+d3d12_video_encoder_get_feedback(struct pipe_video_codec *codec, void *feedback, unsigned *size, struct pipe_enc_feedback_metadata* metadata);
 
 /**
  * end encoding of the current frame

src/gallium/drivers/r600/radeon_vce.c
@@ -352,7 +352,8 @@ static void rvce_end_frame(struct pipe_video_codec *encoder,
 }
 
 static void rvce_get_feedback(struct pipe_video_codec *encoder,
-			      void *feedback, unsigned *size)
+			      void *feedback, unsigned *size,
+				  struct pipe_enc_feedback_metadata* metadata)
 {
 	struct rvce_encoder *enc = (struct rvce_encoder*)encoder;
 	struct rvid_buffer *fb = feedback;

src/gallium/drivers/radeonsi/radeon_uvd_enc.c
@@ -257,7 +257,7 @@ static void radeon_uvd_enc_destroy(struct pipe_video_codec *encoder)
 }
 
 static void radeon_uvd_enc_get_feedback(struct pipe_video_codec *encoder, void *feedback,
-                                        unsigned *size)
+                                        unsigned *size, struct pipe_enc_feedback_metadata* metadata)
 {
    struct radeon_uvd_encoder *enc = (struct radeon_uvd_encoder *)encoder;
    struct rvid_buffer *fb = feedback;

src/gallium/drivers/radeonsi/radeon_vce.c
@@ -322,7 +322,8 @@ static void rvce_end_frame(struct pipe_video_codec *encoder, struct pipe_video_b
    }
 }
 
-static void rvce_get_feedback(struct pipe_video_codec *encoder, void *feedback, unsigned *size)
+static void rvce_get_feedback(struct pipe_video_codec *encoder, void *feedback, unsigned *size,
+                              struct pipe_enc_feedback_metadata* metadata)
 {
    struct rvce_encoder *enc = (struct rvce_encoder *)encoder;
    struct rvid_buffer *fb = feedback;

src/gallium/drivers/radeonsi/radeon_vce_52.c
@@ -85,7 +85,7 @@ static void get_task_info_param(struct rvce_encoder *enc)
    enc->enc_pic.ti.offset_of_next_task_info = 0xffffffff;
 }
 
-static void get_feedback_buffer_param(struct rvce_encoder *enc)
+static void get_feedback_buffer_param(struct rvce_encoder *enc, struct pipe_enc_feedback_metadata* metadata)
 {
    enc->enc_pic.fb.feedback_ring_size = 0x00000001;
 }
@@ -141,7 +141,7 @@ void si_vce_52_get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_
    get_motion_estimation_param(enc, pic);
    get_pic_control_param(enc, pic);
    get_task_info_param(enc);
-   get_feedback_buffer_param(enc);
+   get_feedback_buffer_param(enc, NULL);
    get_vui_param(enc, pic);
    get_config_ext_param(enc);
 

sonphantrung/mesa

commit 64cc624fd03616f086cc91767c209e6326e848b3

frontends/va: Fix leak when destroying VAEncCodedBufferType
Fixes: be4287c3aa0 ("pipe: Extend get_feedback with additional metadata")
Reviewed-by: Sil Vilerino <sivileri@microsoft.com>

src/gallium/frontends/va/buffer.c
@@ -316,7 +316,7 @@ vlVaDestroyBuffer(VADriverContextP ctx, VABufferID buf_id)
 
    if (buf->type == VAEncCodedBufferType) {
       VACodedBufferSegment* node = buf->data;
-      while(!node) {
+      while (node) {
          VACodedBufferSegment* next = (VACodedBufferSegment*) node->next;
          FREE(node);
          node = next;